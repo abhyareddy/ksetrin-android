328ee77bccebd2e442d487bcf9445357d03e5a6a
Sumanth Perambuduri <35676632+supersu-man@users.noreply.github.com>
2022-02-10 10:38:20 +0530

    Pre-alpha 3
    - Added News API
    - Better weather Icon

--- app/src/main/java/org/ksetrin/ksetrin/fragments/HomeFragment.kt
+++ app/src/main/java/org/ksetrin/ksetrin/fragments/HomeFragment.kt
@@ -10,22 +10,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.fondesa.kpermissions.allGranted
 import com.fondesa.kpermissions.coroutines.sendSuspend
 import com.fondesa.kpermissions.extension.permissionsBuilder
+import com.github.pwittchen.weathericonview.WeatherIconView
 import com.google.android.gms.location.LocationServices
-import com.squareup.picasso.Picasso
 import kotlinx.coroutines.*
 import org.json.JSONObject
 import org.ksetrin.ksetrin.R
 import org.ksetrin.ksetrin.RemindersData
 import org.ksetrin.ksetrin.adapters.RemindersAdapter
 import java.util.*
+import com.github.pwittchen.weathericonview.library.R.*
+import org.json.JSONStringer
 
 
 class HomeFragment : Fragment() {
@@ -39,7 +40,7 @@ class HomeFragment : Fragment() {
     private lateinit var humidityTextView: TextView
     private lateinit var visibilityTextView: TextView
     private lateinit var recyclerView: RecyclerView
-    private lateinit var weatherIcon: ImageView
+    private lateinit var weatherIcon: WeatherIconView
 
 
     override fun onCreateView(
@@ -55,7 +56,8 @@ class HomeFragment : Fragment() {
             requireActivity().getSharedPreferences("PREFERENCES", Context.MODE_PRIVATE)
         initViews()
         modifyViews()
-        getWeatherData()
+
+        getSetWeather()
     }
 
     private fun initViews() {
@@ -77,38 +79,40 @@ class HomeFragment : Fragment() {
         recyclerView.layoutManager = linearManager
         val a = RemindersData("Title1", "Timeleft1")
         val b = RemindersData("Title2", "Timeleft2")
-        recyclerView.adapter = RemindersAdapter(mutableListOf(a, b, a))
+        recyclerView.adapter = RemindersAdapter(mutableListOf(a, b, a, b, a))
     }
 
-    private fun getWeatherData() {
+    private fun getSetWeather() = coroutineScope.launch {
         if (!sharedPreferences.contains("weatherData") || isSavedWeatherDataOld()) {
-            getLocation {
-                coroutineScope.launch {
-                    val jsonObject = requestWeatherApi(it.latitude, it.longitude)
-                    jsonObject.put("time", Calendar.getInstance().timeInMillis)
-                    sharedPreferences.edit().putString("weatherData", jsonObject.toString()).apply()
-                    println("Requesting API")
-                    val savedWeatherData = getSavedWeatherData()
-                    updateWeatherInfo(savedWeatherData)
-                }
+            accessLocation  {
+                requestApi(it)
+                println("Requesting API")
             }
         } else {
-            val savedWeatherData = getSavedWeatherData()
-            updateWeatherInfo(savedWeatherData)
+            println("Loading Saved Data")
+            val jsonString = sharedPreferences.getString("weatherData", "")
+            updateWeatherInfo(jsonString.toString())
         }
     }
 
-    private fun updateWeatherInfo(savedWeatherData: JSONObject) =
-        coroutineScope.launch(Dispatchers.Main) {
-            println(savedWeatherData)
-            val temp = savedWeatherData.getJSONObject("main").getDouble("temp") - 273.15
-            val feels_like = savedWeatherData.getJSONObject("main").getDouble("feels_like") - 273.15
-            val wind = savedWeatherData.getJSONObject("wind").getString("speed")
-            val humidity = savedWeatherData.getJSONObject("main").getInt("humidity")
-            val visibility = savedWeatherData.getInt("visibility")
-            val placee = savedWeatherData.getString("name")
-            val iconCode = savedWeatherData.getJSONArray("weather").getJSONObject(0).getString("icon")
-            var iconUrl = "https://openweathermap.org/img/w/$iconCode.png"
+    private fun requestApi(it: Location) = coroutineScope.launch{
+        val response = khttp.get("https://api.openweathermap.org/data/2.5/weather?lat=${it.latitude}&lon=${it.longitude}&appid=${getString(R.string.weather_api_key)}")
+        if (response.statusCode < 400){
+            saveRawData(response.text)
+            updateWeatherInfo(response.text)
+        }
+    }
+
+    private fun updateWeatherInfo(jsonString: String) = coroutineScope.launch(Dispatchers.Main) {
+            val jsonObject = JSONObject(jsonString)
+            val temp = jsonObject.getJSONObject("main").getDouble("temp") - 273.15
+            val feels_like = jsonObject.getJSONObject("main").getDouble("feels_like") - 273.15
+            val wind = jsonObject.getJSONObject("wind").getString("speed")
+            val humidity = jsonObject.getJSONObject("main").getInt("humidity")
+            val visibility = jsonObject.getInt("visibility")
+            val placee = jsonObject.getString("name")
+            val iconCode =
+                jsonObject.getJSONArray("weather").getJSONObject(0).getString("icon")
 
             temperatureTextView.text = String.format("%.1f", temp) + "°"
             feelsLikeTextView.text = "Feels like " + String.format("%.1f", feels_like) + "°"
@@ -116,39 +120,58 @@ class HomeFragment : Fragment() {
             humidityTextView.text = humidity.toString()
             visibilityTextView.text = visibility.toString()
             locationTextView.text = placee
-            Picasso.with(context).load(iconUrl).into(weatherIcon);
+            weatherIcon.setIconResource(getWeatherIcon(iconCode))
         }
 
-    private fun getSavedWeatherData(): JSONObject {
-        val jsonString = sharedPreferences.getString("weatherData", "")
-        return JSONObject(jsonString.toString())
+    private fun getWeatherIcon(iconCode: String): String {
+        when (iconCode) {
+            "01d" -> return getString(string.wi_day_sunny)
+            "02d" -> return getString(string.wi_day_cloudy)
+            "03d" -> return getString(string.wi_cloud)
+            "04d" -> return getString(string.wi_cloudy)
+            "09d" -> return getString(string.wi_day_showers)
+            "10d" -> return getString(string.wi_day_rain)
+            "11d" -> return getString(string.wi_day_thunderstorm)
+            "13d" -> return getString(string.wi_day_snow)
+            "50d" -> return getString(string.wi_dust)
+            "01n" -> return getString(string.wi_night_clear)
+            "02n" -> return getString(string.wi_night_alt_cloudy)
+            "03n" -> return getString(string.wi_cloud)
+            "04n" -> return getString(string.wi_cloudy)
+            "09n" -> return getString(string.wi_day_showers)
+            "10n" -> return getString(string.wi_night_rain)
+            "11n" -> return getString(string.wi_night_thunderstorm)
+            "13n" -> return getString(string.wi_night_snow)
+            "50n" -> return getString(string.wi_dust)
+            else -> return ""
+        }
     }
 
     private fun isSavedWeatherDataOld(): Boolean {
-        val jsonObject = getSavedWeatherData()
+        val jsonString = sharedPreferences.getString("weatherData", "")
+        val jsonObject = JSONObject(jsonString.toString())
         val calendar = Calendar.getInstance()
         return calendar.timeInMillis - jsonObject.getLong("time") > 1800000
     }
 
     @SuppressLint("MissingPermission")
-    private fun getLocation(function: (it: Location) -> Unit) =
-        coroutineScope.launch(Dispatchers.Default) {
-            if (!isPermissionPresent()) {
-                requestMyPermissions()
-            }
-            if (isPermissionPresent()) {
-                val mFusedLocationClient =
-                    LocationServices.getFusedLocationProviderClient(requireActivity())
-                mFusedLocationClient.lastLocation
-                    .addOnSuccessListener {
-                        function(it)
-                    }
-                    .addOnFailureListener {
-                        println("Error getting location")
-                    }
-            }
+    private suspend fun accessLocation(function: (it: Location) -> Unit) {
+        if (!isPermissionPresent()) {
+            requestMyPermissions()
         }
-
+        if (isPermissionPresent()) {
+            val mFusedLocationClient =
+                LocationServices.getFusedLocationProviderClient(requireActivity())
+            mFusedLocationClient.lastLocation
+                .addOnSuccessListener {
+                    function(it)
+                }
+                .addOnFailureListener {
+                    Toast.makeText(requireContext(), "Error getting location", Toast.LENGTH_SHORT)
+                        .show()
+                }
+        }
+    }
 
     private fun isPermissionPresent(): Boolean {
         val permission1 = Manifest.permission.ACCESS_FINE_LOCATION
@@ -158,21 +181,19 @@ class HomeFragment : Fragment() {
         return res1 == PackageManager.PERMISSION_GRANTED && res2 == PackageManager.PERMISSION_GRANTED
     }
 
-    private suspend fun requestMyPermissions(): Boolean {
-        val result = permissionsBuilder(
+    private suspend fun requestMyPermissions() {
+        permissionsBuilder(
             Manifest.permission.ACCESS_COARSE_LOCATION,
             Manifest.permission.ACCESS_FINE_LOCATION
         ).build().sendSuspend()
-        return result.allGranted()
     }
 
-    private fun requestWeatherApi(lat: Double, lng: Double): JSONObject {
-        val jsonString = khttp.get(
-            "https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lng&appid=${
-                getString(R.string.weather_api_key)
-            }"
-        ).text
-        return JSONObject(jsonString)
+    private fun saveRawData(rawdata: String?) {
+        rawdata?.let {
+            val jsonObject = JSONObject(rawdata)
+            jsonObject.put("time", Calendar.getInstance().timeInMillis)
+            sharedPreferences.edit().putString("weatherData", jsonObject.toString()).apply()
+        }
     }
-
 }
+
--- app/src/main/java/org/ksetrin/ksetrin/fragments/NewsFragment.kt
+++ app/src/main/java/org/ksetrin/ksetrin/fragments/NewsFragment.kt
@@ -1,13 +1,25 @@
 package org.ksetrin.ksetrin.fragments
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.gson.JsonObject
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.json.JSONObject
 import org.ksetrin.ksetrin.R
+import java.util.*
+
 
 class NewsFragment : Fragment() {
+
+    private val coroutineScope =  CoroutineScope(Dispatchers.IO)
+    private lateinit var sharedPreferences : SharedPreferences
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,4 +28,43 @@ class NewsFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_news, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        sharedPreferences = requireActivity().getSharedPreferences("PREFERENCES", Context.MODE_PRIVATE)
+        getSetNews()
+    }
+
+    private fun getSetNews() = coroutineScope.launch {
+        val rawdata = getNews()
+        println(rawdata)
+    }
+    
+    private fun getNews(): String? {
+        if(!sharedPreferences.contains("newsData")  || isSavedNewsDataOld()) {
+            println("Requesting API")
+            val response = khttp.get("https://gnews.io/api/v4/search?country=in&lang=en&q=farmer&token=${getString(R.string.news_api_key)}")
+            if (response.statusCode>=400) return null
+            saveRawData(response.text)
+            return response.text
+        } else {
+            println("Loading Saved Data")
+            return sharedPreferences.getString("weatherData", "")
+        }
+    }
+
+    private fun isSavedNewsDataOld(): Boolean {
+        val jsonString = sharedPreferences.getString("weatherData", "")
+        val jsonObject = JSONObject(jsonString.toString())
+        val calendar = Calendar.getInstance()
+        return calendar.timeInMillis - jsonObject.getLong("time") > 3600000
+    }
+
+    private fun saveRawData(rawdata: String?) {
+        rawdata?.let {
+            val jsonObject = JSONObject(rawdata)
+            jsonObject.put("time", Calendar.getInstance().timeInMillis)
+            sharedPreferences.edit().putString("newsData", jsonObject.toString()).apply()
+        }
+    }
 }
