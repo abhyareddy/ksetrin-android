b1def7f4ad21105c84df19602279b2605d2bd381
Sumanth Perambuduri <sumanthpera3@gmail.com>
2022-03-26 18:28:21 +0530

    Pre-alpha 7
    - removed google-services-json
    - added runtime firebase auth
    - updated dependencies
    - added toast messages

--- app/src/main/java/org/ksetrin/ksetrin/activities/LoginActivity.kt
+++ app/src/main/java/org/ksetrin/ksetrin/activities/LoginActivity.kt
@@ -1,10 +1,12 @@
 package org.ksetrin.ksetrin.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
+import android.widget.Toast
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.FirebaseException
@@ -21,7 +23,7 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var sendOtpButton : MaterialButton
     private lateinit var otpEditText : TextInputEditText
     private lateinit var confirmOtpButton : MaterialButton
-    private lateinit var resendOtpButton : MaterialButton
+    //private lateinit var resendOtpButton : MaterialButton
     private lateinit var linearLayout1 : LinearLayout
     private lateinit var linearLayout2 : LinearLayout
 
@@ -44,7 +46,7 @@ class LoginActivity : AppCompatActivity() {
         phoneNumberEditText = findViewById(R.id.loginActivityPhoneInput)
         sendOtpButton = findViewById(R.id.loginActivitySendOtpButton)
         confirmOtpButton = findViewById(R.id.loginActivityConfirmOtpButton)
-        resendOtpButton = findViewById(R.id.loginActivityResendOtpButton)
+        //resendOtpButton = findViewById(R.id.loginActivityResendOtpButton)
         otpEditText = findViewById(R.id.loginActivityOtpInput)
         linearLayout1 = findViewById(R.id.loginActivityLinearLayout1)
         linearLayout2 = findViewById(R.id.loginActivityLinearLayout2)
@@ -54,16 +56,17 @@ class LoginActivity : AppCompatActivity() {
         auth = FirebaseAuth.getInstance()
         callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks(){
             override fun onVerificationCompleted(p0: PhoneAuthCredential) {
-                println(p0)
                 println("On Verification Complete")
+                Toast.makeText(this@LoginActivity, "Verification Complete", Toast.LENGTH_SHORT).show()
                 signInWithPhoneAuthCredential(p0)
             }
             override fun onVerificationFailed(p0: FirebaseException) {
-                println(p0)
                 println("On Verification Failed")
+                Toast.makeText(this@LoginActivity, "Verification Failed", Toast.LENGTH_SHORT).show()
             }
             override fun onCodeSent(verificationId: String, token: PhoneAuthProvider.ForceResendingToken) {
                 println("On Code Sent")
+                Toast.makeText(this@LoginActivity, "Code successfully sent", Toast.LENGTH_SHORT).show()
                 storedVerificationId = verificationId
                 resendToken = token
                 goNextScreen()
@@ -75,12 +78,16 @@ class LoginActivity : AppCompatActivity() {
         sendOtpButton.setOnClickListener {
             startPhoneNumberVerification(phoneNumberEditText.text.toString())
         }
+
+        /*
         resendOtpButton.setOnClickListener {
             resendVerificationCode(storedVerificationId, resendToken)
         }
+         */
 
         confirmOtpButton.setOnClickListener {
-            verifyPhoneNumberWithCode(storedVerificationId, otpEditText.text.toString())
+            val credential = PhoneAuthProvider.getCredential(storedVerificationId, otpEditText.text.toString())
+            signInWithPhoneAuthCredential(credential)
         }
     }
 
@@ -104,21 +111,17 @@ class LoginActivity : AppCompatActivity() {
         PhoneAuthProvider.verifyPhoneNumber(options)
     }
 
-    private fun verifyPhoneNumberWithCode(verificationId: String, code: String) {
-        val credential = PhoneAuthProvider.getCredential(verificationId, code)
-        println(credential)
-    }
-
     private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
         auth.signInWithCredential(credential)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     println("signInWithCredential:success")
-                    val user = task.result?.user
-                    println(user)
-
+                    Toast.makeText(this@LoginActivity, "Signed successfully", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
                 } else {
                     println("signInWithCredential:failure")
+                    Toast.makeText(this@LoginActivity, "Unable to Sign-in", Toast.LENGTH_SHORT).show()
                 }
             }
     }
--- app/src/main/java/org/ksetrin/ksetrin/activities/RoutingActivity.kt
+++ app/src/main/java/org/ksetrin/ksetrin/activities/RoutingActivity.kt
@@ -5,7 +5,11 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.google.firebase.FirebaseOptions
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.initialize
+import org.ksetrin.ksetrin.R
 
 class RoutingActivity : Activity(){
 
@@ -13,18 +17,20 @@ class RoutingActivity : Activity(){
         val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         splashScreen.setKeepOnScreenCondition { true }
-        val sharedPreferences = getSharedPreferences("PREFERENCES", Context.MODE_PRIVATE)
+        val options = FirebaseOptions.Builder()
+            .setProjectId(getString(R.string.project_id))
+            .setApplicationId(getString(R.string.google_app_id))
+            .setApiKey(getString(R.string.google_api_key))
+            .build()
+        Firebase.initialize(this, options, "secondary")
         val user = FirebaseAuth.getInstance().currentUser
         if (user != null) {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         } else {
             val intent = Intent(this, LoginActivity::class.java)
-            //val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
-        if (sharedPreferences.getBoolean("loggedIn", false)){}
-        else{}
         finish()
     }
 }
